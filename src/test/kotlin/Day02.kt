9a0084b09b94f2de4187cfd20746aff4ffce7e8d
Christof Vollrath <christof.vollrath@spring-media.de>
2018-12-03 16:50:34 +0100

    Day 02, part 1

---
+++ src/test/kotlin/Day02.kt
@@ -0,0 +1,107 @@
+
+import org.amshove.kluent.`should equal`
+import org.amshove.kluent.shouldEqual
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on as onData
+
+/*
+
+--- Day 2: Inventory Management System ---
+You stop falling through time, catch your breath, and check the screen on the device.
+"Destination reached. Current Year: 1518. Current Location: North Pole Utility Closet 83N10."
+You made it! Now, to find those anomalies.
+
+Outside the utility closet, you hear footsteps and a voice. "...I'm not sure either.
+But now that so many people have chimneys, maybe he could sneak in that way?"
+Another voice responds, "Actually, we've been working on a new kind of suit
+that would let him fit through tight spaces like that.
+But, I heard that a few days ago, they lost the prototype fabric, the design plans, everything!
+Nobody on the team can even seem to remember important details of the project!"
+
+"Wouldn't they have had enough fabric to fill several boxes in the warehouse?
+They'd be stored together, so the box IDs should be similar.
+Too bad it would take forever to search the warehouse for two similar box IDs..."
+They walk too far away to hear any more.
+
+Late at night, you sneak to the warehouse - who knows what kinds of paradoxes you could cause if you were discovered -
+and use your fancy wrist device to quickly scan every box and produce a list of the likely candidates
+(your puzzle input).
+
+To make sure you didn't miss any, you scan the likely candidate boxes again,
+counting the number that have an ID containing exactly two of any letter and then separately
+counting those with exactly three of any letter.
+You can multiply those two counts together to get a rudimentary checksum and compare it to what your device predicts.
+
+For example, if you see the following box IDs:
+
+abcdef contains no letters that appear exactly two or three times.
+bababc contains two a and three b, so it counts for both.
+abbcde contains two b, but no letter appears exactly three times.
+abcccd contains three c, but no letter appears exactly two times.
+aabcdd contains two a and two d, but it only counts once.
+abcdee contains two e.
+ababab contains three a and three b, but it only counts once.
+
+Of these box IDs, four of them contain a letter which appears exactly twice,
+and three of them contain a letter which appears exactly three times.
+Multiplying these together produces a checksum of 4 * 3 = 12.
+
+What is the checksum for your list of box IDs?
+
+*/
+
+
+fun hasRepeatedChars(input: String, nr: Int)  = input.groupBy { it }.values.any { it.size == nr }
+
+fun countBoxIdsWithRepeatedChars(input: List<String>,  nr: Int) = input.filter { hasRepeatedChars(it, nr) }.count()
+
+fun boxIdsChecksum(input: List<String>) = countBoxIdsWithRepeatedChars(input, 2) * countBoxIdsWithRepeatedChars(input, 3)
+
+class Day2Spec : Spek({
+
+    describe("part 1") {
+        given("example input") {
+            val input = listOf("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab")
+
+            it("should calculate checksum") {
+                boxIdsChecksum(input) `shouldEqual` 12
+            }
+        }
+        describe("hasRepeatedChars") {
+            val testData = arrayOf(
+                    data("abcdef",false, false),
+                    data("bababc",true,  true),
+                    data("abbcde",true,  false),
+                    data("abcccd",false, true),
+                    data("aabcdd",true,  false),
+                    data("abcdee",true,  false),
+                    data("ababab",true,  true)
+            )
+
+            onData("call for action %s", with = *testData) { input, expected2, expected3 ->
+                val result2 = hasRepeatedChars(input, 2)
+                val result3 = hasRepeatedChars(input, 3)
+                it("returns $expected2 $expected3") {
+                    result2 `should equal` expected2
+                    result3 `should equal` expected3
+                }
+            }
+        }
+
+        describe("exercise") {
+            given("exercise input") {
+                val input = readResource("day02Input.txt").split('\n')
+                it("should calculate correct result") {
+                    val result = boxIdsChecksum(input)
+                    result `should equal` 5727
+                }
+
+            }
+        }
+    }
+
+})
