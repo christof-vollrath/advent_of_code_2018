6675e7a7a13224b590fc8e20dd4ee00f89a100c5
Christof Vollrath <christof.vollrath@spring-media.de>
2018-12-21 15:35:01 +0100

    Day07 part 2

--- src/test/kotlin/Day07.kt
+++ src/test/kotlin/Day07.kt
@@ -62,6 +62,52 @@ So, in this example, the correct order is CABDFE.
 
 In what order should the steps in your instructions be completed?
 
+--- Part Two ---
+
+As you're about to begin construction, four of the Elves offer to help.
+"The sun will set soon; it'll go faster if we work together."
+Now, you need to account for multiple people working on steps simultaneously.
+If multiple steps are available, workers should still begin them in alphabetical order.
+
+Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on.
+So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds.
+No time is required between steps.
+
+To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers)
+and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds).
+Then, using the same instructions as above, this is how each second would be spent:
+
+Second   Worker 1   Worker 2   Done
+   0        C          .
+   1        C          .
+   2        C          .
+   3        A          F       C
+   4        B          F       CA
+   5        B          F       CA
+   6        D          F       CAB
+   7        D          F       CAB
+   8        D          F       CAB
+   9        D          .       CABF
+  10        E          .       CABFD
+  11        E          .       CABFD
+  12        E          .       CABFD
+  13        E          .       CABFD
+  14        E          .       CABFD
+  15        .          .       CABFDE
+
+Each row represents one second of time.
+The Second column identifies how many seconds have passed as of the beginning of that second.
+Each worker column shows the step that worker is currently doing (or . if they are idle).
+The Done column shows completed steps.
+
+Note that the order of the steps has changed;
+this is because steps now take time to finish and multiple workers can begin multiple steps simultaneously.
+
+In this example, it would take 15 seconds for two workers to complete these steps.
+
+With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
+
+
  */
 
 class Instructions {
@@ -115,6 +161,37 @@ fun parseInstructions(input: String): Instructions {
         result.add(values[1][0], values[2][0])
     }
     return result
+}
+
+data class Work(val step: Step, val time: Int, var progress: Int = 0)
+
+fun completeSteps(instructions: Instructions, nrWorkers: Int, baseTime: Int): Int {
+    fun calculateTime(step: Char) = baseTime + (step - 'A') + 1
+    var currentWork = instructions.start.map { Work(it, calculateTime(it.id)) }
+    var executing = emptyList<Work>()
+    val alreadyExecuted = mutableListOf<Char>()
+    var totalTime = 0
+    while(true) {
+        currentWork = currentWork.flatMap { work ->
+            if (work.progress == work.time) { // completed
+                alreadyExecuted.add(work.step.id)
+                work.step.outGoing.mapNotNull { nextStep ->
+                    if (currentWork.none { it.step.id == nextStep }) Work(instructions.stepMap[nextStep]!!, calculateTime(nextStep))
+                    else null // already started
+                }
+            } else listOf(work)
+        }
+        if (currentWork.isEmpty()) return totalTime
+        executing = executing.filter { it.time > it.progress } // completed
+        if (executing.size < nrWorkers) {
+            val executable = currentWork.filter { work ->
+                work.step.inGoing.all { it in alreadyExecuted }
+            }.sortedBy { it.step.id }
+            executing += executable.filter { it !in executing }.take(nrWorkers - executing.size) // add new workers
+        }
+        executing.forEach { it.progress++ }
+        totalTime++
+    }
 }
 
 class Day07Spec : Spek({
@@ -173,6 +250,32 @@ class Day07Spec : Spek({
             val exerciseInput = readResource("day07Input.txt")
             val input = parseInstructions(exerciseInput)
             traverseSteps(input).joinToString("") `should equal` "BCADPVTJFZNRWXHEKSQLUYGMIO"
+        }
+    }
+    describe("part 2") {
+        given("example") {
+            val input = """Step C must be finished before step A can begin.
+                    Step C must be finished before step F can begin.
+                    Step A must be finished before step B can begin.
+                    Step A must be finished before step D can begin.
+                    Step B must be finished before step E can begin.
+                    Step D must be finished before step E can begin.
+                    Step F must be finished before step E can begin.
+                """.trimIndent()
+            val instructions = parseInstructions(input)
+            it("should take 60*6+21 seconds for a single worker") {
+                completeSteps(instructions, 1, 60) `should equal` 381
+            }
+            it("should take 15 seconds for a two workers and zero base time") {
+                completeSteps(instructions, 2, 0) `should equal` 15
+            }
+        }
+        given("exercise") {
+            val exerciseInput = readResource("day07Input.txt")
+            val instructions = parseInstructions(exerciseInput)
+            it("should find solution") {
+                completeSteps(instructions, 5, 60) `should equal` 973
+            }
         }
     }
 })
