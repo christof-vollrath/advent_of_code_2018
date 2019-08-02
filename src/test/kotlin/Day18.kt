7f119c3455033e0d5bd092a96f0b720acdd35a32
Christof Vollrath <christof-vollrath@spring-media.de>
2019-08-02 15:34:26 +0200

    Day18 part 1 - solved

--- src/test/kotlin/Day18.kt
+++ src/test/kotlin/Day18.kt
@@ -3,6 +3,7 @@ import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.amshove.kluent.`should equal`
+import org.jetbrains.spek.api.dsl.on
 
 /*
 --- Day 18: Settlers of The North Pole ---
@@ -174,8 +175,88 @@ Multiplying the number of wooded acres by the number of lumberyards gives the to
 
 What will the total resource value of the lumber collection area be after 10 minutes?
 
-
  */
+
+fun parseLumberArea(input: String): LumberArea {
+    val areaTypes = input.split("\n")
+            .map { line ->
+                line.map {
+                    areaTypeValueOf(it)
+                }.toTypedArray()
+            }.toTypedArray()
+    return LumberArea(areaTypes)
+}
+
+fun areaTypeValueOf(c: Char) = enumValues<AreaType>().first { it.c == c }
+
+enum class AreaType(val c: Char) {
+    OPEN('.'),
+    TREES('|'),
+    LUMBER('#');
+
+    override fun toString() = c.toString()
+    fun executeMinute(adjacentAreaTypeCounts: Map<AreaType, Int>) =
+            when(this) {
+                OPEN ->
+                    if ((adjacentAreaTypeCounts[TREES] ?: 0) >= 3) TREES
+                    else OPEN
+                TREES ->
+                    if ((adjacentAreaTypeCounts[LUMBER] ?: 0) >= 3) LUMBER
+                    else TREES
+                LUMBER ->
+                    if ((adjacentAreaTypeCounts[LUMBER] ?: 0) >= 1 && (adjacentAreaTypeCounts[TREES] ?: 0) >= 1) LUMBER
+                    else OPEN
+            }
+}
+
+class LumberArea(val grid: Array<Array<AreaType>>) {
+    override fun toString() =
+            grid.joinToString("") { row ->
+                row.joinToString("", postfix = "\n") { element -> element.toString() }
+            }
+
+    fun get(x: Int, y: Int) = grid[y][x]
+    fun executeMinute(): LumberArea {
+        val nextGrid = grid.mapIndexed { y , row ->
+            row.mapIndexed { x, area ->
+                val adjacentAreaTypeCounts = countAdjacentAreaTypes(x, y)
+                area.executeMinute(adjacentAreaTypeCounts)
+            }.toTypedArray()
+        }.toTypedArray()
+        return LumberArea(nextGrid)
+    }
+
+    fun countAdjacentAreaTypes(x: Int, y: Int): Map<AreaType, Int> =
+            countAreaTypes(adjacentAreas(x, y))
+
+    fun countAreaTypes(areaTypes: List<AreaType>) = areaTypes.groupBy { it }.map { entry -> entry.key to entry.value.size }.toMap()
+
+    fun countAreaTypes() = with(grid.flatMap { row -> row.map { it } }) {
+        countAreaTypes(this)
+    }
+
+    fun adjacentAreas(x: Int, y: Int): List<AreaType> =
+            (-1..1).flatMap { dy ->
+                (-1..1).mapNotNull { dx ->
+                    val x2 = x + dx
+                    val y2 = y + dy
+                    when {
+                        dx == 0 && dy == 0 -> null
+                        x2 < 0 -> null
+                        y2 < 0 -> null
+                        y2 >= grid.size -> null
+                        x2 >= grid[y2].size -> null
+                        else -> get(x2, y2)
+                    }
+                }
+            }
+
+    fun executeMinutes(n: Int): LumberArea = if (n == 0) this
+    else executeMinute().executeMinutes(n - 1)
+
+    fun totalResources() = with(countAreaTypes()) { (this[AreaType.TREES] ?: 0) * (this[AreaType.LUMBER] ?: 0) }
+
+}
 
 class Day18Spec : Spek({
 
@@ -213,40 +294,76 @@ class Day18Spec : Spek({
                             |||||.||.|
                             ||||||||||
                             ....||..|.
+                            
                             """.trimIndent()
                 }
+            }
+            describe("10 minutes") {
+                on("execute action of ten minutes") {
+                    val result = input.executeMinutes(10)
+                    it("should execute action of ten minutes") {
+                        result.toString() `should equal` """
+                            .||##.....
+                            ||###.....
+                            ||##......
+                            |##.....##
+                            |##.....##
+                            |##....##|
+                            ||##.####|
+                            ||#####|||
+                            ||||#|||||
+                            ||||||||||
+                            
+                            """.trimIndent()
+                    }
+                    it("should count areas") {
+                        val counts = result.countAreaTypes()
+                        counts[AreaType.TREES] `should equal` 37
+                        counts[AreaType.LUMBER] `should equal` 31
+                    }
+                    it("should calculate total resources") {
+                        result.totalResources() `should equal` 1147
+                    }
+
+                }
+            }
+            describe("count adjacent areas") {
+                it("should get adjacent areas") {
+                    input.adjacentAreas(6, 1) `should equal` listOf(
+                            AreaType.OPEN, AreaType.OPEN, AreaType.TREES,
+                            AreaType.LUMBER, AreaType.LUMBER,
+                            AreaType.OPEN, AreaType.OPEN, AreaType.OPEN
+                            )
+                }
+                it("should count adjacent areas for an area in the middle") {
+                    input.countAdjacentAreaTypes(6, 1) `should equal` mapOf(
+                            AreaType.OPEN to 5,
+                            AreaType.TREES to 1,
+                            AreaType.LUMBER to 2
+                    )
+                }
+                it("should count adjacent areas at the upper left boarder") {
+                    input.countAdjacentAreaTypes(9, 9) `should equal` mapOf(
+                            AreaType.OPEN to 1,
+                            AreaType.TREES to 2
+                    )
+                    it("should count adjacent areas at the lower right boarder") {
+                        input.countAdjacentAreaTypes(0, 0) `should equal` mapOf(
+                                AreaType.OPEN to 2,
+                                AreaType.LUMBER to 1
+                        )
+                    }
+                }
+            }
+        }
+        given("exercise input") {
+            val inputString = readResource("day18Input.txt")
+            val input = parseLumberArea(inputString)
+            val result = input.executeMinutes(10)
+            println(result)
+            it("should calculate total resources") {
+                result.totalResources() `should equal` 360720
             }
         }
     }
 })
-
-fun parseLumberArea(input: String): LumberArea {
-    val areaTypes = input.split("\n")
-            .map { line ->
-                line.map {
-                    areaTypeValueOf(it)
-                }.toTypedArray()
-            }.toTypedArray()
-    return LumberArea(areaTypes)
-}
-
-fun areaTypeValueOf(c: Char) = enumValues<AreaType>().first { it.c == c }
-
-enum class AreaType(val c: Char) {
-    OPEN('.'),
-    TREES('|'),
-    LUMBER('#');
-
-    override fun toString() = c.toString()
-}
-
-class LumberArea(val grid: Array<Array<AreaType>>) {
-    override fun toString() =
-            grid.joinToString("") { row ->
-                row.joinToString("", postfix = "\n") { element -> element.toString() }
-            }
-
-    fun executeMinute(): LumberArea {
-        return LumberArea(grid)
-    }
-}
