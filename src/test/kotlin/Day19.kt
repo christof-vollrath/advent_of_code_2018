311c91784a906ec91dfbeeb3613438a445a9e7d8
Christof Vollrath <christof-vollrath@spring-media.de>
2019-08-16 16:32:38 +0200

    Day19 - part 2 solved

--- src/test/kotlin/Day19.kt
+++ src/test/kotlin/Day19.kt
@@ -92,6 +92,14 @@ The instruction pointer is incremented, causing it to point outside the program,
 
 What value is left in register 0 when the background process halts?
 
+--- Part Two ---
+
+A new background process immediately spins up in its place.
+It appears identical, but on closer inspection, you notice that this time,
+register 0 started with the value 1.
+
+What value is left in register 0 when this new background process halts?
+
  */
 
 data class IpBinding(val ipRegister: Int)
@@ -119,26 +127,89 @@ fun parseCommandsWithDeclaration(input: String): Pair<IpBinding, List<Command>> 
     }
     return ipBinding to commands
 }
-
-fun executeCommands(ipBinding: IpBinding, commands: List<Command>): List<Int> {
-    var registers = List(6) { 0 }
+typealias ExecutionHook = (Command, List<Int>, Int) -> Boolean
+fun executeCommands(ipBinding: IpBinding, commands: List<Command>, startRegisters: List<Int> = List(6) { 0 },
+                    preExecutionHook: ExecutionHook? = null): List<Int> {
+    var registers =  startRegisters
     var ip = 0
     while(ip >= 0 && ip < commands.size) {
-        val (nextIp, nextRegisters) = executeCommand(ip, ipBinding, registers, commands)
+        val command = commands[ip]
+        if (preExecutionHook != null) {
+            val breakExecution = preExecutionHook(command, registers, ip)
+            if (breakExecution) break
+        }
+        val (nextIp, nextRegisters) = executeCommand(ip, ipBinding, registers, command)
         ip = nextIp; registers = nextRegisters
-        //println("registers=$registers ip=$ip")
     }
     return registers
 }
 
-private fun executeCommand(ip: Int, ipBinding: IpBinding, registers: List<Int>, commands: List<Command>): Pair<Int, List<Int>> {
+private fun executeCommand(ip: Int, ipBinding: IpBinding, registers: List<Int>, command: Command): Pair<Int, List<Int>> {
     val inputRegisters = registers as MutableList
     inputRegisters[ipBinding.ipRegister] = ip
-    val command = commands[ip]
     val changedRegisters = command.execute(inputRegisters)
     val nextIp = changedRegisters[ipBinding.ipRegister] + 1
     return nextIp to changedRegisters
 }
+
+/*
+#ip 3
+00 addi 0 16 IP  +
+01 seti 1 - R5   |         < <  R5 = 1
+02 seti 1 - R2   |       < | |
+03 mulr R5 R2 R1 |     < | | |
+04 eqrr R1 R4 R1 |     | | | |  if (R1 == R4)
+05 addr R1 IP IP | +   | | | |
+06 addi 6 1 IP   | --+ | | | |
+07 addr R5 R0 R0 | < | | | | |  R0 += R5
+08 addi R2 1 R2  | < - | | | |  R2++
+09 gtrr R2 R4 R1 |     | | | |  if (R2 > R4)
+10 addr 10 R1 IP | +   | | | |
+11 seti 2 - IP   | ----+ | | |
+12 addi R5 1 R5  | <     | | |  R5++
+13 gtrr R5 R4 R1 |       | | |  if (R5 > R4)
+14 addr R1 14 IP | +     | | |  |
+15 seti 1 - IP   |   ----+ | |  |
+16 mulr 16 16 IP |         | |  - Stop
+17 addi R4 2 R4  <         | |  R4 += 2
+18 mulr R4 R4 R4           | |  R4 = R4 ** 2
+19 mulr 19 R4 R4           | |  R4 *= 19
+20 muli R4 11 R4           | |  R4 *= 11
+21 addi R1 6 R1            | |  R1 += 6
+22 mulr R1 22 R1           | |  R1 *= 22
+23 addi R1 21 R1           | |  R1 += 21
+24 addr R4 R1 R4           | |  R4 += R1
+25 addr 25 R0 IP +         | |  if (R0 > 0)
+26 seti 0 - IP   ----------+ |
+27 setr 27 - R1  <           |  -
+28 mulr R1 28 R1             |  R1 *= 28
+29 addr 29 R1 R1             |  R1 += 29
+30 mulr 30 R1 R1             |  R1 *= 30
+31 muli R1 14 R1             |  R1 *= 14
+32 mulr R1 32 R1             |  R1 *= 32
+33 addr R4 R1 R4             |  R4 += R1
+34 seti 0 - R0               |  R0 = 0
+35 seti 0 - IP   ------------+
+ */
+
+fun decompiledMainLoop(r4: Int): Int { // Lines 01 - 16
+    var r0 = 0 // 34
+    var r5 = 1 // 01
+    do {
+        var r2 = 1 // 02
+        do {
+            val r1 = r5 * r2 // 03
+            if (r1 == r4) { // 04, 05, 06 r1 is divisor of r4
+                r0 += r5 // 07
+            }
+            r2++ // 08
+        } while (!(r2 > r4)) // 09, 10, 11
+        r5++ // 12
+    } while (!(r5 > r4)) // 13, 14, 15
+    return r0
+}
+
+fun divisors(n: Int) = (1..n).filter {n % it == 0 }
 
 class Day19Spec : Spek({
 
@@ -179,15 +250,20 @@ class Day19Spec : Spek({
                     seti 9 0 5
                 """.trimIndent()
                 val commandsWithDeclaration = parseCommandsWithDeclaration(input)
-                val result = executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second)
                 it("should execute commands correctly") {
+                    val result = executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second)
                     result `should equal` listOf(6, 5, 6, 0, 0, 9)
                 }
                 it("should have the correct value in register 0") {
+                    val result = executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second)
                     result[0] `should equal` 6
                 }
             }
-
+        }
+        describe("find divisors") {
+            it("should calculate divisors for 989") {
+                divisors(989) `should equal` listOf(1, 23, 43, 989)
+            }
         }
         describe("exercise") {
             given("exercise input") {
@@ -197,8 +273,69 @@ class Day19Spec : Spek({
                     val result = executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second)
                     result[0] `should equal` 1056
                 }
+                it("should execute some commands with trace output") {
+                    var executionCounter = 0
+                    var trace = ""
+                    executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second) { cmd, registers, ip ->
+                        trace += "cmd=$cmd registers=$registers ip=$ip\n"
+                        executionCounter++
+                        executionCounter >= 10
+                    }
+                    trace `should equal` """
+                        cmd=Addi(a=3, b=16, c=3) registers=[0, 0, 0, 0, 0, 0] ip=0
+                        cmd=Addi(a=4, b=2, c=4) registers=[0, 0, 0, 16, 0, 0] ip=17
+                        cmd=Mulr(a=4, b=4, c=4) registers=[0, 0, 0, 17, 2, 0] ip=18
+                        cmd=Mulr(a=3, b=4, c=4) registers=[0, 0, 0, 18, 4, 0] ip=19
+                        cmd=Muli(a=4, b=11, c=4) registers=[0, 0, 0, 19, 76, 0] ip=20
+                        cmd=Addi(a=1, b=6, c=1) registers=[0, 0, 0, 20, 836, 0] ip=21
+                        cmd=Mulr(a=1, b=3, c=1) registers=[0, 6, 0, 21, 836, 0] ip=22
+                        cmd=Addi(a=1, b=21, c=1) registers=[0, 132, 0, 22, 836, 0] ip=23
+                        cmd=Addr(a=4, b=1, c=4) registers=[0, 153, 0, 23, 836, 0] ip=24
+                        cmd=Addr(a=3, b=0, c=3) registers=[0, 153, 0, 24, 989, 0] ip=25
+                        
+                    """.trimIndent()
+                }
+                it("should calculate the value of R4 before entering the main loop in line 01") {
+                    var r4 = 0
+                    executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second) { _, registers, ip ->
+                        val enteringLoop = ip == 1
+                        if (enteringLoop) {
+                            r4 = registers[4]
+                        }
+                        enteringLoop
+                    }
+                    r4 `should equal` 989
+                }
+                it("should calculate same value using decompiled function starting with 989") {
+                    decompiledMainLoop(989) `should equal` 1056
+                }
+                it("should calculate the result much faster") {
+                    divisors(989).sum() `should equal` 1056
+                }
             }
-
         }
     }
+    describe("part 2") {
+        given("exercise input") {
+            val inputString = readResource("day19Input.txt")
+            val commandsWithDeclaration = parseCommandsWithDeclaration(inputString)
+            val startRegisters = listOf(1) + List(5) { 0 }
+            it("should calculate the value of R4 before entering the main loop in line 01") {
+                var r4 = 0
+                executeCommands(commandsWithDeclaration.first, commandsWithDeclaration.second, startRegisters) { _, registers, ip ->
+                    val enteringLoop = ip == 1
+                    if (enteringLoop) {
+                        r4 = registers[4]
+                    }
+                    enteringLoop
+                }
+                r4 `should equal` 10551389
+            }
+            it("should calculate the result much faster") {
+                divisors(10551389).sum() `should equal` 10915260
+            }
+        }
+
+    }
+
 })
